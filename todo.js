a0408f9ec6cc6bbd527cef6d2939dab2334679a9
MarkoVolic <markovolic16@gmail.com>
2023-08-29 10:45:46 -0400

    working on the delete button

--- todo.js
+++ todo.js
@@ -1,21 +1,38 @@
 function onClick(){
     var add = document.getElementById("myText").value;
     var node = document.createElement('li');
-    node.appendChild(document.createTextNode(add));
+    var but = document.createElement('button');
+
+    but.style.cssText += ' background-color: red;border: none;color: white;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;border-radius: 12px;';
     
+
+
+    node.appendChild(document.createTextNode(add));
+    but.appendChild(document.createTextNode('-'));
+
+
     document.querySelector('ul').appendChild(node); 
-    }
-    // Get the elements by their ID
-    var popupLink = document.getElementById("button");
-    var popupWindow = document.getElementById("popup-window");
-    var closeButton = document.getElementById("close-button");
-    // Show the pop-up window when the link is clicked
-    popupLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      popupWindow.style.display = "block";
-    });
-    // Hide the pop-up window when the close button is clicked
-    closeButton.addEventListener("click", function() {
-        onClick();
-      popupWindow.style.display = "none";
-    });
+    document.querySelector('ul').appendChild(but); 
+
+}
+
+function Delete(){
+    
+}
+
+
+// Get the elements by their ID
+var popupLink = document.getElementById("button");
+var popupWindow = document.getElementById("popup-window");
+var closeButton = document.getElementById("close-button");
+// Show the pop-up window when the link is clicked
+popupLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+});
+// Hide the pop-up window when the close button is clicked
+closeButton.addEventListener("click", function() {
+    onClick();
+    popupWindow.style.display = "none";
+});
+    
